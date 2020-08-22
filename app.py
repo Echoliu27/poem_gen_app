593a58e8581b809150e4447637606580469c7020
Echoliu27 <bl199@duke.edu>
2020-08-22 14:46:57 -0400

    Add second topic selection feature

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ Things to do:
 # import gpt2 library
 import gpt_2_simple as gpt2
 import os
+import random
+import pandas as pd
+import numpy as np
 # import flask library
 from flask import Flask, request, render_template
 app = Flask(__name__)
@@ -27,21 +30,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	# can automate this use POS tagging
+	curated_topic_list = ['love','dog','cat','life','friend','school','spring','summer','basketball',
+							'rain', 'mom', 'blue', 'snow', 'brother', 'sky', 'winter', 'family', 'football',
+							'sister', 'bird', 'red', 'stork', 'baseball', 'dream', 'tree', 'crazy', 'soccer', 
+							'flowers', 'christmas', 'wind', 'wish', 'fall', 'homework', 'puppy', 'bed', 'eyes',
+							'fish', 'teacher', 'eat', 'baby', 'moon', 'green', 'shoes', 'beach', 'ice', 'mouse',
+							'candy', 'monkey', 'stars', 'heart', 'bear', 'frog', 'hair', 'house', 'people', 'fly',
+							'dad', 'food', 'game', 'pizza', 'nature', 'beautiful', 'roses', 'friendship', 'colors',
+							'halloween', 'happy', 'animals', 'ocean', 'sports', 'hands', 'pig', 'pie', 'books', 'horse',
+							'war', 'monster', 'forever', 'sweet', 'dark', 'ode', 'music', 'water', 'dragon', 'awoke',
+							'feet', 'butterfly', 'white', 'hate', 'chicken', 'rat', 'bunny', 'sea', 'hockey', 
+							'pencil', 'cheese', 'chocolate', 'write', 'car', 'fire', 'dance', 'feelings', 'bananas',
+							'pickles', 'angel','kitty', 'weird', 'today', 'rainbow', 'seasons', 'leaves', 'lunch', 'bee',
+							'snake', 'birthday', 'computer', 'hat', 'tiger', 'sad', 'earth', 'death', 'money', 'story', 'pink',
+							'math', 'magic', 'lion', 'jelly', 'bike', 'america', 'turtle', 'frogs', 'darkness', 'teddy', 'tears',
+							'thunder', 'scary']
+
+	return render_template('home.html', data = random.sample(curated_topic_list, 10))
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	# input
-	int_text = request.form['sentence']
-
-	# preprocess input (what if input are not words at all?) Only deal with empty input for now
-	# lowercase input
-	if (len(int_text) != 0):
-		final_text = '<|startoftext|> ' + int_text.lower()
-		output = ['']
-	else:
-		error = "Empty input. You have not fed the hungry machine yet."
-
+	
+	final_text, error = get_input(request.form)
+	output = ['']
 
 	# start the gpt2 session
 	sess = gpt2.start_tf_sess()
@@ -49,7 +61,7 @@ def predict():
 	               run_name="run1",
 	               checkpoint_dir="checkpoint")
 
-	while(len(output[0]) <= len(final_text)):
+	while(len(output[0]) <= len(final_text) + 30):
 		output = gpt2.generate(sess, 
 	              run_name='run1',
 	              checkpoint_dir='checkpoint',
@@ -64,8 +76,45 @@ def predict():
 	              )
 
 
-	return render_template('home.html', prediction_poem = output[0].replace('<|startoftext|> ', ''))
+	return render_template('home.html', prediction_poem = output[0].replace('<|startoftext|> ', ''), error = error)
 	# HAN: if you want to generate a new page => redirect(url_for(...))
+
+
+
+def get_input(request_dict):
+
+	# not very rigorious error logic yet
+	error = ''
+	if (request_dict.get('oneTopic') == None and request_dict.get('sentence') != None):
+		'''
+		1. User type in one sentence
+		
+		'''
+		# user input (haven't limit number of characters for input)
+		int_text = request.form['sentence']
+
+		# preprocess input, only deal with empty input for now
+		final_text = '<|startoftext|> ' + int_text.lower()
+		
+	elif (request_dict.get('oneTopic') != None):
+		'''
+		2. User choose one topic
+		
+		'''
+		# title and first sentence csv file
+		df = pd.read_csv('data/poem_title_sentence.csv')
+		df = df.dropna()
+	
+		# user input
+		topic = request.form['oneTopic']
+
+		# search for the first sentence corresponding to the topic in csv
+		int_text = random.sample(df[df['title_clean'].str.contains(topic)]['first_sentence'].to_list(), 1)
+		final_text = '<|startoftext|> ' + int_text[0]
+	else:
+		error = 'Empty input. You have not fed the hungry machine yet.'
+	return final_text, error
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ data_preprocess.py
@@ -0,0 +1,50 @@
+'''
+
+Generate a pandas pd with cleaned title and cleaned first sentence of a poem.
+
+'''
+import numpy as np
+import pandas as pd
+import re
+from html import unescape
+
+def rm_html(raw):
+    return(unescape(str(raw)).lower()) #not all objects are string, some maybe float
+    
+def rm_nonalphabet(raw):
+    pre1 = re.sub(r'[^a-zA-Z\s\d]','', raw) #not all objects are string, some maybe float
+    pre2 = re.sub(r'[\s]{2,}', ' ', pre1)
+    return pre2
+
+def clean_text(raw):
+    pre1 = re.sub(r'[^\x01-\x7F]+','', raw) #ascii index 1-127 is alphanumeric + space
+    pre2 = re.sub('â', '', pre1)
+    pre3 = re.sub(r'[\s]{2,}', ' ', pre2)
+    return pre3
+
+def first_sentence_tokenized(poem):
+    return re.split(r'[,.!]', poem)[0]
+
+
+if __name__ == '__main__':
+    # read in data
+    poem_data = 'data/poki.csv'
+    df = pd.read_csv(poem_data)
+
+    # clean title
+    df['title_clean'] = df['title'].apply(rm_html)
+    df['title_clean'] = df['title_clean'].apply(rm_nonalphabet)
+
+    # clean text and create first_sentence
+    df['text_clean'] = df['text'].apply(clean_text)
+    df['first_sentence'] = df['text_clean'].apply(first_sentence_tokenized)
+
+    # create cleaned pd dataframe
+    df_cleaned = df.filter(['title_clean','first_sentence'], axis = 1)
+    
+    # when the first sentence exceeds 50 characters, drop them
+    final_df = df_cleaned[df_cleaned['first_sentence'].str.len() <= 50]
+    final_df.head()
+    final_df.to_csv('data/poem_title_sentence.csv', index = False)
+
+
